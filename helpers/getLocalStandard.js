f8acb635d9d895bafcac597b1c0449e139cc9e5b
just-paja <pavel@zak.global>
2020-08-18 10:30:29 +0200

    Upgrade find-up

--- helpers/getLocalStandard.js
+++ helpers/getLocalStandard.js
@@ -2,6 +2,6 @@ const path = require('path')
 const findUp = require('find-up')
 
 exports.getLocalStandard = rootDir => {
-  const nodeModulesPath = findUp.sync('node_modules', { cwd: rootDir })
+  const nodeModulesPath = findUp.sync('node_modules', { cwd: rootDir, type: 'directory' })
   return require(path.resolve(nodeModulesPath, 'standard'))
 }
